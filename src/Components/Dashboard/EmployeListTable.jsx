e92762603ac1bc9270a498dd0062ad760fc42b97
Nadir Hossain <nadirhossain336@gmail.com>
2024-03-26 13:44:25 +0600

    create custom table

--- src/Components/Dashboard/EmployeListTable.jsx
+++ src/Components/Dashboard/EmployeListTable.jsx
@@ -42,31 +42,6 @@ const data = [
 ];
 
 const EmployeListTable = () =>{
-  
-
-  const columns = [
-      {
-        title: "Serial No",
-        dataIndex: "key",
-        key: "key",
-      },
-      {
-        title: "Company Name",
-        dataIndex: "companyname",
-        key: "company",
-      },
-      {
-        title: "Category",
-        dataIndex: "category",
-        key: "category",
-      },
-      
-      {
-        title: "Location",
-        dataIndex: "location",
-        key: "location",
-      }
-  ];
 
   return(
     <div className="h-fit rounded-[8px] bg-white p-6" >
@@ -76,12 +51,42 @@ const EmployeListTable = () =>{
           <p className="text-[#3C64A4] text-[12px] underline">VIEW ALL</p>
         </Link>
       </div>
-      <Table 
-        columns={columns} 
-        dataSource={data?.slice(0, 4)} 
-        pagination={false}
-        className="employee-table"
-      />
+
+      <table className="w-full rounded-[5px] rounded-table">
+        <tr className="text-left w-full bg-[#FEE3B8]" style={{borderRadius: "8px"}}>
+          {
+            ["Serial No", "Company Name", "Category", "Location"].map((item, index)=>
+              <th
+                style={{
+                  borderTopLeftRadius: item === "Serial No" ? "8px" : 0,
+                  borderBottomLeftRadius: item === "Serial No" ? "8px" : 0,
+                  borderTopRightRadius: item === "Location" ? "8px" : 0,
+                  borderBottomRightRadius: item === "Location" ? "8px" : 0,
+                }} 
+                key={index} 
+                className="py-[10px] px-10"
+              >
+                {item}
+              </th>
+            )
+          }
+        </tr>
+
+        
+        {
+          (data?.slice(0, 4))?.map((item, index)=>
+          <>
+            <div key={index} style={{marginTop: '8px'}}></div>
+            <tr key={index} className="bg-[#F2F8FE]" >
+              <td className="py-[10px] rounded-[5px] pl-10">{item.key}</td>
+              <td className="py-[10px] pl-10">{item.companyname}</td>
+              <td className="py-[10px] pl-10">{item.category}</td>
+              <td className="py-[10px] rounded-[5px] pl-10">{item.location}</td>
+            </tr>
+          </>
+          )
+        }
+      </table>
     </div>
   )
 
