fa5e8a4376063412ae22d0944c91f27433cf37b4
Nadir Hossain <nadirhossain336@gmail.com>
2024-05-11 18:14:45 +0600

    :hammer: refactor code

--- Config.jsx
+++ Config.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const baseURL = axios.create({
-    baseURL: "http://192.168.10.64:8000/api",
+    // baseURL: "http://192.168.10.64:8000/api",
+    baseURL: "https://app.findwork.careers/api",
     timeout: 10000,
     headers: {'X-Custom-Header': 'foobar'}
   });
--- ImgConfig.jsx
+++ ImgConfig.jsx
@@ -1,2 +1,3 @@
-const ImgURL= "http://192.168.10.64:8000";
+// const ImgURL= "http://192.168.10.64:8000";
+const ImgURL= "https://app.findwork.careers";
 export default ImgURL;
