42900b36be47401115a85e6235144a14b6192825
Nadir Hossain <nadirhossain336@gmail.com>
2024-05-13 11:45:59 +0600

    :hammer: change img config

--- src/Pages/Main/Dashboard.jsx
+++ src/Pages/Main/Dashboard.jsx
@@ -267,7 +267,7 @@ const Dashboard = () => {
             <Link to={`${userType === "RECRUITER" ? "/employer-profile" : "/settings/profile"}`}>
               <div className="w-[171px] h-[42px] bg-[#ffffff] rounded-[5px] flex items-center gap-3 p-[10px]">
                 <img 
-                  src={ image ? `${ImgURL}/${image}` : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLotvhr2isTRMEzzT30Cj0ly77jFThGXr0ng&usqp=CAU" }   
+                  src={ image ? `${ImgURL}${image}` : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLotvhr2isTRMEzzT30Cj0ly77jFThGXr0ng&usqp=CAU" }   
                   style={{
                     width: "30px", 
                     height: "30px", 
