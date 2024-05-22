420c4edb123be4cc0f4397b2092e336da3b0f5fb
Nadir Hossain <nadirhossain336@gmail.com>
2024-05-22 17:32:25 +0600

    :art: complete

--- Config.jsx
+++ Config.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const baseURL = axios.create({
-    baseURL: "http://192.168.10.64:8000/api",
-    // baseURL: "https://app.findwork.careers/api",
+    // baseURL: "http://192.168.10.64:8000/api",
+    baseURL: "https://app.findwork.careers/api",
     timeout: 10000,
     headers: {'X-Custom-Header': 'foobar'}
   });
