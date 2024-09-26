741a793191a82be257336881cb8d4bcd45717704
Nadir Hossain <nadirhossain336@gmail.com>
2024-09-26 16:03:42 +0600

    refactor: :recycle: refactor api

--- ImgConfig.jsx
+++ ImgConfig.jsx
@@ -1,4 +1,5 @@
 // const ImgURL= "http://192.168.10.64:8000";
-const ImgURL= "https://app.findwork.careers";
+// const ImgURL= "https://app.findwork.careers";
+const ImgURL= "https://server.remedycare.co.uk";
 // const ImgURL= "http://152.42.204.147:8000";
 export default ImgURL;
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: "192.168.10.102",
+    // host: "192.168.10.195",
     host: "152.42.204.147",
     port: "3001",
   },
